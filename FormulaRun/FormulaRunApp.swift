3f253e4442c2c868ebd79ee79a74030cef2a214c
Siddartha Tummala <sidddddd@amazon.com>
2025-04-05 11:39:16 -0700

    Initial Commit

---
+++ FormulaRun/FormulaRunApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormulaRunApp.swift
+//  FormulaRun
+//
+//  Created by Tummala, Siddartha on 4/5/25.
+//
+
+import SwiftUI
+
+@main
+struct FormulaRunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FormulaRunTests/FormulaRunTests.swift
@@ -0,0 +1,17 @@
+//
+//  FormulaRunTests.swift
+//  FormulaRunTests
+//
+//  Created by Tummala, Siddartha on 4/5/25.
+//
+
+import Testing
+@testable import FormulaRun
+
+struct FormulaRunTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
